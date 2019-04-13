b3bb02eefe186cef3ae6b7a34a55c26abb26fec0
Andre Sottil <dreso2423@gmail.com>
2019-04-13 17:57:03 -0500

    new

--- db/schema.rb
+++ db/schema.rb
@@ -12,13 +12,6 @@
 
 ActiveRecord::Schema.define(version: 2019_04_13_224941) do
 
-  create_table "blods", force: :cascade do |t|
-    t.string "title"
-    t.text "body"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
   create_table "blogs", force: :cascade do |t|
     t.string "title"
     t.text "body"
